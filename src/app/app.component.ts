a2e8e27e8db2abaa908f4208f7bd3cf22b45baba
Eric McGrath <emcgrath.shiftdigital@outlook.com>
2018-05-22 11:09:33 -0400

    Basic HMR support. App now updates in place without browser refresh. Component state will be lost since nothing is done to preserve it. Component styles are reflected in HMR rebuilds, but global styles are not (they need a browser refresh to be seen). This is since the component styles are part of the JS build process and inlined, but global styles are extracted to css files at time of build.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  changeTitle() {
+    // note that this modified state would be lost in an HMR recompilation
+    // unless we do something explicitly to somehow save it before angular is
+    // torn down and then reconstructed.
+    this.title = 'Title changed';
+  }
 }
