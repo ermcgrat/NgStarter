f0d47b5ab97e2dbd6463684647e216979705caf6
Eric McGrath <emcgrath.shiftdigital@outlook.com>
2018-05-25 13:11:10 -0400

    Initial state update

--- src/app/app.state.service.ts
+++ src/app/app.state.service.ts
@@ -6,8 +6,8 @@ export class AppStateService {
 
   // attach various component states to this object
   // We maintain an object for synchronous use by the HMR, and an Observable for use by the application and its templates.
-  private appState: IAppState = {};
-  private appStateSubject = new BehaviorSubject<IAppState>({});
+  private appState: IAppState = { hmrBuilds: 0 };
+  private appStateSubject = new BehaviorSubject<IAppState>({ hmrBuilds: 0 });
   public appState$: Observable<IAppState> = this.appStateSubject.asObservable();
 
   constructor() { }
